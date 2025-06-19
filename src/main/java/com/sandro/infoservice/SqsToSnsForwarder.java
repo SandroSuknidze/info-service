b0ec0756f308cb768eebbaa8943cada1276736e5
Sandro Ubuntu <sandro.suknidze.1@btu.edu.ge>
2025-06-20 01:35:06 +0400

    Refactor `SqsToSnsForwarder` to improve scalability and error handling. Add thread pool for concurrent processing and detailed logging. Adjust SQS polling frequency and update application properties.

--- src/main/java/com/sandro/infoservice/SqsToSnsForwarder.java
+++ src/main/java/com/sandro/infoservice/SqsToSnsForwarder.java
@@ -1,6 +1,9 @@
 package com.sandro.infoservice;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,8 +16,12 @@ import software.amazon.awssdk.services.sqs.model.Message;
 import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
 import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class SqsToSnsForwarder {
 
   private final SqsClient sqsClient;
@@ -27,35 +34,59 @@ public class SqsToSnsForwarder {
   @Value("${aws.sns.topic.arn}")
   private String snsArn;
 
-  @Scheduled(fixedRate = 30000)
-  public void forwardMessages() {
+  @Value("${app.sqs.processor.thread-pool-size:5}")
+  private int threadPoolSize;
+
+  private ExecutorService processingPool;
+
+  @PostConstruct
+  public void init() {
+    processingPool = Executors.newFixedThreadPool(threadPoolSize);
+  }
+
+  @PreDestroy
+  public void shutdown() {
+    processingPool.shutdown();
+    log.info("ExecutorService shut down.");
+  }
+
+  @Scheduled(fixedDelay = 5000)
+  public void pollAndForward() {
     ReceiveMessageResponse response = sqsClient.receiveMessage(
         ReceiveMessageRequest.builder()
             .queueUrl(sqsUrl)
             .maxNumberOfMessages(10)
             .waitTimeSeconds(10)
             .build());
-    for (Message msg : response.messages()) {
+    for (Message message : response.messages()) {
+      processingPool.submit(() -> processMessage(message));
+    }
+  }
+
+  private void processMessage(Message message) {
+    try {
       snsClient.publish(PublishRequest.builder()
           .topicArn(snsArn)
           .subject("New Image Uploaded")
-          .message(buildNotificationMessage(msg.body()))
-//          .message(msg.body())
-          .build());
+          .message(buildNotificationMessage(message.body())).build());
 
-      sqsClient.deleteMessage(DeleteMessageRequest.builder()
-          .queueUrl(sqsUrl)
-          .receiptHandle(msg.receiptHandle())
-          .build());
+      sqsClient.deleteMessage(
+          DeleteMessageRequest.builder()
+              .queueUrl(sqsUrl)
+              .receiptHandle(message.receiptHandle())
+              .build());
+
+      log.info("Message forwarded & deleted: {}", message.messageId());
+
+    } catch (Exception e) {
+      log.error("Error processing message {}: {}", message.messageId(), e.getMessage());
     }
   }
 
   private String buildNotificationMessage(String json) {
     JSONObject obj = new JSONObject(json);
     return String.format("An image was uploaded!\nName: %s\nSize: %s bytes\nType: %s\nDownload: %s",
-        obj.getString("fileName"),
-        obj.getInt("size"),
-        obj.getString("extension"),
+        obj.getString("fileName"), obj.getInt("size"), obj.getString("extension"),
         obj.getString("downloadUrl"));
   }
 }
